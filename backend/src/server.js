4563d85f83878e96e1d4c991ec3d489f480c4813
roman <adhikariroman01m@gmail.com>
2026-06-24 15:13:10 +0545

    add Dockerfile and .dockerignore; implement server static file serving and health check endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,38 @@
 import express from "express";
 import "dotenv/config";
+import fs from "fs"; //file system
+import path from "path";
 
 import User from "../models/user.model.js";
 import { connectDB } from "../lib/db.js";
 
 import cors from "cors";
-import { clerkMiddleware } from '@clerk/express';
+import { clerkMiddleware } from "@clerk/express";
 
 const app = express();
-const PORT=process.env.PORT;
-const FRONTEND_URL=process.env.FRONTEND_URL;
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public"); //join the current working directory and find the public folder
 
 app.use(express.json);
-app.use(cors({ origin:FRONTEND_URL, credentials:true}));
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(clerkMiddleware);
 
-app.get("/health", (req,res) => {
-    res.status(200).json({ ok: true});
-})
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true });
+});
 
+//FOR DEPLOYMENT
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {   //Does the public folder exists? (only true in produciton).
+  app.use(express.static(publicDir)); //this is our react application converted into static assets. serves the built React files
+  app.get("/{*any}", (req, res, next) => {   //any other URL ->send back to index.html
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 app.listen(PORT, () => {
-    connectDB();
-    console.log("Server is running on PORT:", PORT);
+  connectDB();
+  console.log("Server is running on PORT:", PORT);
 });
