2c8b5cde730748cf47406b042ee1bf3071d6f78e
roman <adhikariroman01m@gmail.com>
2026-07-03 13:56:16 +0545

    - Updated main.jsx to include BrowserRouter for routing. -Created AuthPage and ChatPage components for future routing. -Removed vite.svg asset as it is no longer needed. - Updated index.css to integrate Tailwind CSS and HeroUl styles. - Configured Vite to use Tailwind CSS plugin.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
-import "./App.css";
+import {ThemeProvider} from "./context/ThemeContext";
+import {WallpaperProvider} from "./context/WallpaperContext";
+import { Navigate, Route, Routes } from "react-router";
 
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import ChatPage from "./pages/Chatpage";
+import AuthPage from "./pages/AuthPage";
+import { useAuth } from "@clerk/react";
 
 function App() {
+  const {isSignedIn, isLoaded} = useAuth();
+  //TODO: make this component better.
+  if(!isLoaded) return <p>Loading...</p> 
+
   return (
-    <div>
-      MY APP
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+      <WallpaperProvider>
+      <Routes>
+        <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to="/auth" replace />} />
+        <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace />} />
+      </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   );
 }
 
